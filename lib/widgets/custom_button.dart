8f303c6226d0065b4553a16dc2191f316ca85d3d
abraralidev <abraraliiofficial@gmail.com>
2022-10-27 22:03:58 +0500

    init

---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,41 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:flutter/material.dart';
+import 'package:videoapp/utils/colors.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+  const CustomButton({
+    Key? key,
+    required this.text,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(18.0),
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          primary: buttonColor,
+          minimumSize: const Size(
+            double.infinity,
+            50,
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+            side: const BorderSide(color: buttonColor),
+          ),
+        ),
+        child: Text(
+          text,
+          style: const TextStyle(
+            fontSize: 17,
+          ),
+        ),
+      ),
+    );
+  }
+}
